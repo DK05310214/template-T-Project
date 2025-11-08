50f455bc64906a25b93375241fb0b12abc3e92c2
anpeng.duo <duoap@qq.com>
2025-11-08 21:12:54 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include "spdlog/spdlog.h"
 // #include <Eigen/Eigen>
+#include <Eigen/Eigen>
 
 int main(int, char**){
     std::cout << "Hello, from test!\n";
     spdlog::info("Hello, from spdlog!");
 
-    // Eigen::MatrixXf matrix1(3,4);
-    // Eigen::Vector3f vector1;
+    Eigen::MatrixXf matrix1(3,4);
+    Eigen::Vector3f vector1;
 
-    // matrix1 = Eigen::MatrixXf::Zero(3,4);
-    // vector1 = Eigen::Vector3f::Ones();
+    matrix1 = Eigen::MatrixXf::Zero(3,4);
+    vector1 = Eigen::Vector3f::Ones();
 
-    // std::cout << "------ matrix1 ------" << std::endl << matrix1 << std::endl;
-    // std::cout << "------ vector1 ------" << std::endl << vector1 << std::endl;
+    std::cout << "------ matrix1 ------" << std::endl << matrix1 << std::endl;
+    std::cout << "------ vector1 ------" << std::endl << vector1 << std::endl;
 }
